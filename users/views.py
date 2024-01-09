780f41e1b4611f1ed549af828b28503ba70725b3
ImanGholami <imangholamiimi@gmail.com>
2024-01-09 14:52:01 +0330

    UPDATE | VIEWS | :zap:

--- users/views.py
+++ users/views.py
@@ -1,9 +1,13 @@
-from django.contrib.auth import get_user_model
-from django.shortcuts import render
+from django.contrib.auth import get_user_model, logout, login, authenticate
+from django.contrib.auth.views import LoginView
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views import View
 from django.views.generic import CreateView
 from django.contrib.auth.models import Group
 from django.urls import reverse, reverse_lazy
 from users.forms import customUserCreationForm
+from users.models import UserProfile
 
 
 # Create your views here.
@@ -27,3 +31,44 @@ class SignUpView(CreateView):
         user.save()
         user.groups.add(group)
         return response
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            next_url = request.session.get('next')
+            if next_url:
+                del request.session['next']
+                return redirect(next_url)
+            return redirect('/home')
+        else:
+            return render(request, 'users/login.html')
+
+    next_url = request.GET.get('next')
+    if next_url:
+        request.session['next'] = next_url
+
+    return render(request, 'users/login.html')
+
+
+class Logout(View):
+    def get(self, request):
+        if self.request.user:
+            latest_user_login = request.user.username
+            logout(request)
+            response = redirect('home')
+            response.set_cookie('latest_user_login', latest_user_login)
+            return response
+
+def profile_view(request):
+    user_profile = UserProfile.objects.get(user=request.user)
+    user_addresses = user_profile.addresses.all()
+    context = {'user_profile': user_profile, 'user_addresses': user_addresses}
+    return render(request, 'users/profile.html', context)
+
+
+def home(request):
+    return HttpResponse(f"Ola {request.user.username.upper()} ! 😊")
