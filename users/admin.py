397359626606cdf65d5c6744bab747d3e462ea47
ImanGholami <imangholamiimi@gmail.com>
2024-01-09 14:50:44 +0330

    UPDATE | ADIMIN | :zap:

--- users/admin.py
+++ users/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, Permission
-from users.models import User, Employee, Customer, UserProfile
+from users.models import User, Employee, Customer, UserProfile, Address
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Employee)
 admin.site.register(Customer)
 admin.site.register(UserProfile)
+admin.site.register(Address)
 
-# admin.site.unregister(Group)  # این کد گروه‌های از پیش تعریف شده را از admin panel حذف می‌کند
-
+admin.site.unregister(Group)  # این کد گروه‌های از پیش تعریف شده را از admin panel حذف می‌کند
+#
 Group.objects.get_or_create(name='Customer')
 Group.objects.get_or_create(name='Employee')
 
