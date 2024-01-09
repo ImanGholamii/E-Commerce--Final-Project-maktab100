599366b93072c170ecb4971012c47185dd14960b
ImanGholami <imangholamiimi@gmail.com>
2024-01-09 14:51:49 +0330

    UPDATE | URLS | :zap:

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from users.views import SignUpView
+from users.views import SignUpView, login_view, profile_view, Logout, home
+
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name="signup"),
+    path('login/', login_view, name="login"),
+    path('logout/', Logout.as_view(), name="logout"),
+    path('users/profile/', profile_view , name="profile"),
+    path('home/', home , name="home"),
 ]
+
