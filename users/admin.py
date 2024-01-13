8cd464c41e70fe3fadb154998c9dff1962ad0190
ImanGholami <imangholamiimi@gmail.com>
2024-01-13 19:40:08 +0330

    UPDATE | USERS | ADMIN | :zap:

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, Permission
 from users.models import User, Employee, Customer, UserProfile, Address
+from django.utils.translation import gettext_lazy as _
+
+
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'user_type', 'is_active', 'is_staff', 'date_joined')
+    search_fields = ('id', 'username', 'email')
+    list_filter = ('user_type', 'is_active', 'is_staff', 'date_joined')
+    ordering = ('-date_joined',)
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'city', 'state', 'street', 'postal_code', 'is_default')
+    search_fields = ('id', 'user__username', 'city', 'state', 'street', 'postal_code')
+    list_filter = ('is_default',)
+
+
 admin.site.register(User)
 admin.site.register(Employee)
 admin.site.register(Customer)
@@ -9,12 +25,12 @@ admin.site.register(UserProfile)
 admin.site.register(Address)
 
 admin.site.unregister(Group)  # این کد گروه‌های از پیش تعریف شده را از admin panel حذف می‌کند
-#
-Group.objects.get_or_create(name='Customer')
-Group.objects.get_or_create(name='Employee')
 
-customer_group = Group.objects.get(name='Customer')
-employee_group = Group.objects.get(name='Employee')
+Group.objects.get_or_create(name=_('Customer'))
+Group.objects.get_or_create(name=_('Employee'))
+
+customer_group = Group.objects.get(name=_('Customer'))
+employee_group = Group.objects.get(name=_('Employee'))
 
 permissions = Permission.objects.filter(name__icontains='product')
 customer_group.permissions.add(*permissions)
