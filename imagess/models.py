7b4b15419df1174a7b7074094dd197b819e92caf
ImanGholami <imangholamiimi@gmail.com>
2024-01-13 19:16:50 +0330

    UPDATE | IMAGESS | MODELS | :zap: | Add Settings of Translation

--- imagess/models.py
+++ imagess/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from products.models import Product
 
+
 class ProductImage(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='product_images/')
-    is_default = models.BooleanField(default=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images', verbose_name=_('Product'))
+    image = models.ImageField(upload_to='product_images/', verbose_name=_('Image'))
+    is_default = models.BooleanField(default=False, verbose_name=_('Is Default'))
 
     def __str__(self):
         return f"Image for {self.product.name}"
