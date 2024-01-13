eea67a8a961957d57cd3b9c11a51f48f29ca609e
ImanGholami <imangholamiimi@gmail.com>
2024-01-13 19:42:09 +0330

    UPDATE | USERS | FORMS | :zap: | Add Settings of Translation

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 
-class customUserCreationForm(UserCreationForm):
-    # user_type = forms.ChoiceField(choices=(('customer', 'Customer'), ('employee', 'Employee')))
+class CustomUserCreationForm(UserCreationForm):
+    user_type = forms.ChoiceField(choices=(('customer', _('Customer')), ('employee', _('Employee'))))
+
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ['username', 'email', 'phone', 'gender', 'date_of_birth',
