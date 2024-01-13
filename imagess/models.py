d7885cba9b14df1e1c68e6e15d01c89c2484cf51
ImanGholami <imangholamiimi@gmail.com>
2024-01-13 18:49:59 +0330

    ADD | MODELS | :tada:

--- imagess/models.py
+++ imagess/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from products.models import Product
 
-# Create your models here.
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='product_images/')
+    is_default = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Image for {self.product.name}"
+
+    class Meta:
+        verbose_name = _("Product Image")
+        verbose_name_plural = _("Product Images")
