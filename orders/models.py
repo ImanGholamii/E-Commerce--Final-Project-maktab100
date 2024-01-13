009468fd12ac3b846ed4b147699b7f24c1452f56
ImanGholami <imangholamiimi@gmail.com>
2024-01-13 19:19:06 +0330

    UPDATE | ORDERS | MODELS | :zap: | Add Settings of Translation

--- orders/models.py
+++ orders/models.py
@@ -2,35 +2,43 @@ from django.db import models
 from users.models import Customer, Employee
 from products.models import Product
 from core.models import TimeStampBaseModel, LogicalBaseModel
+from django.utils.translation import gettext_lazy as _
+
+
 class Order(TimeStampBaseModel, LogicalBaseModel):
     STATUS_CHOICES = [
-        ('pending', 'Pending'),
-        ('processing', 'Processing'),
-        ('completed', 'Completed'),
-        ('cancelled', 'Cancelled'),
+        ('pending', _('Pending')),
+        ('processing', _('Processing')),
+        ('completed', _('Completed')),
+        ('cancelled', _('Cancelled')),
     ]
 
-    employee = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True, related_name='employee_orders')
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='customer_orders')
-    order_date = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    employee = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True, related_name='employee_orders',
+                                 verbose_name=_('Employee'))
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='customer_orders',
+                                 verbose_name=_('Customer'))
+    order_date = models.DateTimeField(auto_now_add=True, verbose_name=_('Order Date'))
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending', verbose_name=_('Status'))
 
     def __str__(self):
         return f"Order #{self.id} - {self.status}"
 
+
 class OrderItem(TimeStampBaseModel, LogicalBaseModel):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantities = models.PositiveIntegerField()
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items', verbose_name=_('Order'))
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name=_('Product'))
+    quantities = models.PositiveIntegerField(verbose_name=_('Quantities'))
 
     def __str__(self):
-        return f"Order #{self.order.id} - {self.product.name} - Quantity: {self.quantity}"
+        return f"Order #{self.order.id} - {self.product.name} - Quantity: {self.quantities}"
+
 
 class OrderHistory(TimeStampBaseModel, LogicalBaseModel):
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True, related_name='history')
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    status = models.CharField(max_length=20, choices=Order.STATUS_CHOICES)
-    time = models.DateTimeField(auto_now_add=True, auto_now=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True, related_name='history',
+                              verbose_name=_('Order'))
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, verbose_name=_('Customer'))
+    status = models.CharField(max_length=20, choices=Order.STATUS_CHOICES, verbose_name=_('Status'))
+    time = models.DateTimeField(auto_now_add=True, auto_now=True, verbose_name=_('Time'))
 
     def __str__(self):
         return f"Order #{self.order.id} - Status: {self.status} - Timestamp: {self.time}"
