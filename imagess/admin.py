cd64ea9bfd7d1681835548685e93156379682cb4
ImanGholami <imangholamiimi@gmail.com>
2024-01-15 00:53:52 +0330

    UPDATE | ADMIN | IMAGESS | :zap:

--- imagess/admin.py
+++ imagess/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from imagess.models import ProductImage, ProductImages
 # Register your models here.
+admin.site.register(ProductImage)
+admin.site.register(ProductImages)
