42b935350260dfd72bcdac58a64d1456f279272a
ImanGholami <imangholamiimi@gmail.com>
2024-01-15 01:05:32 +0330

    UPDATE | ADMIN | PRODUCTS | :zap:

--- products/admin.py
+++ products/admin.py
@@ -1,25 +1,24 @@
 from django.contrib import admin
-from .models import Brand, Category, Product, Discount, PromoCode, Comment, ParentChildComment
+from .models import Brand, Category, Product, Discount, PromoCode, Comment, ParentChildComment, ProductCategory
 from django.utils.html import format_html
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'price', 'brand', 'display_image', 'display_categories')
+    list_display = ('name', 'description', 'display_image', 'price', 'brand', 'display_categories')
     raw_id_fields = ('category',)
-
-    list_display = ('name', 'description', 'display_image', 'price', 'brand')
 
     def display_image(self, obj):
         images = obj.images.all()
         if images:
             image_url = images[0].image.url if hasattr(images[0].image, 'url') else "No Image"
             if image_url:
-                return format_html('<img src="{}" width="100" height="100" />', image_url)
+                return format_html('<div style="text-align: center;"><img src="{}" width="100" height="100" /></div>'.format(image_url))
         return "No Image"
 
     display_image.short_description = 'Image'
 
     def display_categories(self, obj):
-        return ', '.join([category.name for category in obj.category.all()])
+        categories = obj.productcategory_set.all()
+        return ', '.join([category.category.name for category in categories])
 
     display_categories.short_description = 'Categories'
 
@@ -31,3 +30,4 @@ admin.site.register(ParentChildComment)
 admin.site.register(Discount)
 admin.site.register(PromoCode)
 admin.site.register(Brand)
+admin.site.register(ProductCategory)
