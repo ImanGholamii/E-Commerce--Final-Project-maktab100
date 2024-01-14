482ef4d3015356ce05aef9881dae6ca4419d50b2
ImanGholami <imangholamiimi@gmail.com>
2024-01-15 00:54:26 +0330

    UPDATE | IMAGESS | MODELS | :zap:

--- imagess/models.py
+++ imagess/models.py
@@ -4,13 +4,29 @@ from products.models import Product
 
 
 class ProductImage(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images', verbose_name=_('Product'))
     image = models.ImageField(upload_to='product_images/', verbose_name=_('Image'))
     is_default = models.BooleanField(default=False, verbose_name=_('Is Default'))
+
+    @property
+    def url(self):
+        return self.image.url
+
+    def __str__(self):
+        image_name = self.image.name.split("/")[-1]
+        return f"Image {self.id}: 😋 {image_name}"
+
+    class Meta:
+        verbose_name = _("Product Image")
+        verbose_name_plural = _("Product Image")
+
+
+class ProductImages(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images', verbose_name=_('Product'))
+    image = models.ForeignKey(ProductImage, on_delete=models.CASCADE, verbose_name=_('Image'))
 
     def __str__(self):
         return f"Image for {self.product.name}"
 
     class Meta:
-        verbose_name = _("Product Image")
+        verbose_name = _("Product Images")
         verbose_name_plural = _("Product Images")
