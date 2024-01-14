f3961ce58c73fbb21bf5450e89be7db14c5486f6
ImanGholami <imangholamiimi@gmail.com>
2024-01-15 01:37:28 +0330

    UPDATE | ADMIN | PRODUCTS | :zap:

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.utils.translation import activate
 from .models import Brand, Category, Product, Discount, PromoCode, Comment, ParentChildComment, ProductCategory
 from django.utils.html import format_html
+from django.utils.translation import gettext_lazy as _
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'display_image', 'price', 'brand', 'display_categories')
@@ -14,20 +16,25 @@ class ProductAdmin(admin.ModelAdmin):
                 return format_html('<div style="text-align: center;"><img src="{}" width="100" height="100" /></div>'.format(image_url))
         return "No Image"
 
-    display_image.short_description = 'Image'
+    display_image.short_description = _('Image')
 
     def display_categories(self, obj):
         categories = obj.productcategory_set.all()
         return ', '.join([category.category.name for category in categories])
 
-    display_categories.short_description = 'Categories'
+    display_categories.short_description = _('Categories')
 
+    actions = ['switch_to_persian']
+
+    def switch_to_persian(self, request, queryset):
+        activate('fa')
+    switch_to_persian.short_description = _('Switch to Persian')
 
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Category)
-admin.site.register(Comment)
-admin.site.register(ParentChildComment)
-admin.site.register(Discount)
-admin.site.register(PromoCode)
-admin.site.register(Brand)
-admin.site.register(ProductCategory)
+admin.site.register(Category, admin.ModelAdmin)
+admin.site.register(Comment, admin.ModelAdmin)
+admin.site.register(ParentChildComment, admin.ModelAdmin)
+admin.site.register(Discount, admin.ModelAdmin)
+admin.site.register(PromoCode, admin.ModelAdmin)
+admin.site.register(Brand, admin.ModelAdmin)
+admin.site.register(ProductCategory, admin.ModelAdmin)
