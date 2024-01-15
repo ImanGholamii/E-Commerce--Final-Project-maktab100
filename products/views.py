01d38fed30ac60f8cf4e497eec4c72b0d26bfca3
ImanGholami <imangholamiimi@gmail.com>
2024-01-15 13:50:34 +0330

    UPDATE | PRODUCTS | VIEWS | :zap:

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Product
 
-# Create your views here.
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        product = self.get_object()
+        context['product_images'] = product.images.all()
+        context['category_path'] = product.category.get().get_full_path()
+        return context
