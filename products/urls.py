9d4cbf6446f80726ac7f8758974c54d1d500546b
ImanGholami <imangholamiimi@gmail.com>
2024-01-16 19:09:43 +0330

    UPDATE | URLS | PRODUCTS | :zap:

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from products.views import ProductDetailView
+from products.views import ProductDetailView, template_render
+
 urlpatterns = [
+    path('', template_render, name='index'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='detail'),
 ]
