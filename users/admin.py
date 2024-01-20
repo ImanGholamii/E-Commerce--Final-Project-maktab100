5b3336af5dffb058f668e502df5a68a46b2eab17
ImanGholami <imangholamiimi@gmail.com>
2024-01-21 01:27:22 +0330

    UPDATE | USERS | ADMIN | :zap:

--- users/admin.py
+++ users/admin.py
@@ -6,25 +6,25 @@ from django.utils.translation import gettext_lazy as _
 
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'email', 'is_customer', 'is_active', 'is_staff', 'date_joined')
-    search_fields = ('id', 'username', 'email')
-    list_filter = ('is_customer', 'is_active', 'is_staff', 'date_joined')
+    list_display = ('id', 'username', 'email', 'phone', 'is_customer', 'is_active', 'is_staff', 'date_joined')
+    search_fields = ('id', 'username', 'email', 'phone')
+    list_filter = ('is_customer', 'is_employee', 'is_active', 'is_staff', 'date_joined')
     ordering = ('-date_joined',)
 
 
 class AddressAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'city', 'state', 'street', 'postal_code', 'is_default')
-    search_fields = ('id', 'user__username', 'city', 'state', 'street', 'postal_code')
+    search_fields = ('id', 'user__username', 'city', 'state', 'street', 'postal_code', 'is_default')
     list_filter = ('is_default',)
 
 
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Employee)
 admin.site.register(Customer)
 admin.site.register(UserProfile)
-admin.site.register(Address)
+admin.site.register(Address, AddressAdmin)
 
-admin.site.unregister(Group)  # این کد گروه‌های از پیش تعریف شده را از admin panel حذف می‌کند
+admin.site.unregister(Group)  # این کد گروه‌های از پیش تعریف شده django را از admin panel حذف می‌کند
 
 Group.objects.get_or_create(name=_('Customer'))
 Group.objects.get_or_create(name=_('Employee'))
