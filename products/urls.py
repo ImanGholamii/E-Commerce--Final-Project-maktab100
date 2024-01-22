01fb2539846fa3b1c336b8bc1fdb9819b5a07b44
ImanGholami <imangholamiimi@gmail.com>
2024-01-23 01:44:44 +0330

    ADD | URL | :tada: | Added a main Url using TemplateView

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from products.views import ProductDetailView, template_render
+from products.views import ProductDetailView, template_render, HomeView
 
 urlpatterns = [
-    path('', template_render, name='index'),
+    path('', HomeView.as_view(), name='index'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='detail'),
 ]
