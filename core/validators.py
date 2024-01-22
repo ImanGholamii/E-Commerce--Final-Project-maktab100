fd69ef960b29d852f400c0a67cc66a15a47a7b6d
ImanGholami <imangholamiimi@gmail.com>
2024-01-22 19:27:38 +0330

    UPDATE | VALIDATOR | :zap: | [Phone Valiator | core.validators

--- core/validators.py
+++ core/validators.py
@@ -6,10 +6,21 @@ from django.utils.translation import gettext_lazy as _
 class Validator:
     @staticmethod
     def phone_validator():
-        return RegexValidator(regex=r'^(\+98|0)?9\d{9}$',
-                              message=_("Phone number must be start with +98 or 0 in IR format."),
-                              code=_('invalid IR phone number')
-                              )
+        def validate_phone(value):
+            if get_user_model().objects.filter(phone__iexact=value).exists():
+                raise ValidationError(
+                    _("This phone number is already in use. Please use a different phone number."),
+                    code='duplicate_phone'
+                )
+
+            phone_regex = RegexValidator(
+                regex=r'^(\+98|0)?9\d{9}$',
+                message=_("Phone number must be start with +98 or 0 in IR format."),
+                code='invalid_IR_phone_number'
+            )
+            phone_regex(value)
+
+        return validate_phone
 
     @staticmethod
     def password_validator():
