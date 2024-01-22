dc1415c279fbee87eb86eebc5f77557593775e84
ImanGholami <imangholamiimi@gmail.com>
2024-01-22 13:11:25 +0330

    UPDATE | USERS | FORMS | :zap: | Add customer field check

--- users/forms.py
+++ users/forms.py
@@ -5,14 +5,19 @@ from django.utils.translation import gettext_lazy as _
 
 
 class CustomUserCreationForm(UserCreationForm):
-
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
-        fields = [_('username'), _('email'), _('phone'), _('password1'), _('password2')
+        fields = [_('username'), _('email'), _('phone'), _('password1'), _('password2'), _('is_customer')
                   ]
+        labels = {
+            'is_customer': 'Customer'
+        }
+
+
 
 class UserChangeForm(forms.ModelForm):
     password = ReadOnlyPasswordHashField(help_text="<a href=\"../password/\">Change password</a>")
+
     class Meta:
         model = get_user_model()
         fields = "__all__"
