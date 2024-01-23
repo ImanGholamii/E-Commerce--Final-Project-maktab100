bb3ae00d7c3ffafe21064dd1fd740b743c181d54
ImanGholami <imangholamiimi@gmail.com>
2024-01-23 16:44:17 +0330

    UPDATE | VIEWS | :zap: | Show data in separate pages | :art:

--- products/views.py
+++ products/views.py
@@ -4,25 +4,15 @@ from products.models import Product
 from users.forms import CustomUserCreationForm
 
 
-
 def template_render(request):
     """Temporary view just to show home page"""
     return render(request, 'index.html')
 
-class HomeView(TemplateView):
-    template_name = 'index.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        form = CustomUserCreationForm()
-        context['form'] = form
-
-        return context
 
 class ProductListView(ListView):
     model = Product
-    template_name = 'index.html'
+    template_name = 'products/product_list.html'
+
 
 class ProductDetailView(DetailView):
     """to show product details probably in details page"""
@@ -35,4 +25,19 @@ class ProductDetailView(DetailView):
         product = self.get_object()
         context['product_images'] = product.images.all()
         context['category_path'] = product.category.get().get_full_path()
+        return context
+
+
+class HomeView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        form = CustomUserCreationForm()
+        context['form'] = form
+
+        product_list = Product.objects.all()
+        context['product_list'] = product_list
+
         return context
