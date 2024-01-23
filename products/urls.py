179461c7be484fc9685a13ce81aa93bc74f3297c
ImanGholami <imangholamiimi@gmail.com>
2024-01-23 16:45:18 +0330

    ADD | PRODUCTS | URLS | :tada: | urls for list view

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from products.views import ProductDetailView, template_render, HomeView
+from products.views import ProductDetailView, HomeView, ProductListView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
+    path('products/', ProductListView.as_view(), name='list'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='detail'),
 ]
