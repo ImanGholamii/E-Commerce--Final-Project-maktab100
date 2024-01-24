f72937748b4489eeca8656106333458c4fcb8619
ImanGholami <imangholamiimi@gmail.com>
2024-01-25 02:22:03 +0330

    ADD | USERS | URLS | VERIFY | :tada:

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from users.views import SignUpView, login_view, profile_view, Logout, home
+from users.views import SignUpView, login_view, profile_view, Logout, home, UserRegisterCodeView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name="signup"),
+    path('verify/', UserRegisterCodeView.as_view(), name="verify"),
     path('login/', login_view, name="login"),
     path('logout/', Logout.as_view(), name="logout"),
-    path('users/profile/', profile_view , name="profile"),
+    path('users/profile/', profile_view, name="profile"),
     path('home/', home , name="home"),
 ]
 
