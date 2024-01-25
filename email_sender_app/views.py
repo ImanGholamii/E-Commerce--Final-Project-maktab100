92a383e758d8fbca77ecc337827206433f8e19b1
ImanGholami <imangholamiimi@gmail.com>
2024-01-26 01:12:04 +0330

    ADD | VIEWS | EMAIL_SENDER_APP | :tada: | Add send_email_view

--- email_sender_app/views.py
+++ email_sender_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+def send_email_view(request):
+    if request.method == 'POST':
+        form = EmailForm(request.POST)
+        if form.is_valid():
+            recipient = form.cleaned_data['recipient']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            send_mail(
+                subject,
+                message,
+                'sharlotimi@gmail.com',
+                [recipient],
+                fail_silently=False,
+            )
+            # return render(request, 'success.html')
+            return HttpResponse('Done')
+    else:
+        form = EmailForm()
+    return render(request, 'email_sender/email_form.html', {'form': form})
