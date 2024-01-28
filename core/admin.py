4c51faad128e8f14b8591f6c28e88d1101973832
ImanGholami <imangholamiimi@gmail.com>
2024-01-28 19:17:14 +0330

    UPDATE | CORE | ADMIN | :zap: | Add ADMINSITE SETTINGS | :tada: | login template

--- core/admin.py
+++ core/admin.py
@@ -4,3 +4,6 @@ from django.contrib import admin
 admin.site.index_title = 'Fast Foodia'
 admin.site.site_header = "Fast Foodia Admin 🍔"
 admin.site.site_title = "🍔 Fast Foodia"
+
+class AdminArea(admin.AdminSite):
+    login_template = 'core/admin/login.html'
