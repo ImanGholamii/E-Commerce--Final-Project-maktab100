a368a7e0a959bf52253367ada8825b5d9a3b85e5
ImanGholami <imangholamiimi@gmail.com>
2024-01-28 18:19:11 +0330

    ADD | IMAGES | ADMIN | :tada: | CUSTOMIZATION | SHOW IMAGES | :art:

--- imagess/admin.py
+++ imagess/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from imagess.models import ProductImageAlbum, ProductImages
-# Register your models here.
-admin.site.register(ProductImageAlbum)
-admin.site.register(ProductImages)
+from django.utils.safestring import mark_safe
+from .models import ProductImageAlbum, ProductImages
+
+@admin.register(ProductImageAlbum)
+class ProductImageAlbumAdmin(admin.ModelAdmin):
+    list_display = ('id', 'image_thumbnail', 'is_default')
+    list_display_links = ('id', 'image_thumbnail')
+
+    def image_thumbnail(self, obj):
+        return mark_safe('<img src="{}" width="50" height="50" />'.format(obj.url))
+
+    image_thumbnail.short_description = 'Image'
+
+@admin.register(ProductImages)
+class ProductImagesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'image_thumbnail')
+    list_display_links = ('id', 'product', 'image_thumbnail')
+
+    def image_thumbnail(self, obj):
+        return mark_safe('<img src="{}" width="50" height="50" />'.format(obj.image.url))
+
+    image_thumbnail.short_description = 'Image'
