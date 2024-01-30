b7855bb5fc4e20f1c14cf02f970fdfaaef3ba170
ImanGholami <imangholamiimi@gmail.com>
2024-01-30 14:42:55 +0330

    :rewind: | FIX | :rotating_light: | VIEWS | back to the Jan-25

--- users/views.py
+++ users/views.py
@@ -25,14 +25,12 @@ class SignUpView(CreateView):
     model = get_user_model()
     form_class = CustomUserCreationForm
     template_name = 'users/sign_up.html'
-    # template_name = 'index.html'
-    # success_url = reverse_lazy('login')
     success_url = reverse_lazy('verify')
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         form = self.get_form()
         if form.is_valid():
-            random_code = generate_otp_code()
+            random_code = random.randint(100000, 999999)
             send_otp_code(
                 recipient=form.cleaned_data['email'],
                 subject='FAST FOODIA Verification Code',
@@ -47,8 +45,10 @@ class SignUpView(CreateView):
             }
             messages.success(request, 'OTP Code Sent to your email.', 'success')
             return HttpResponseRedirect(self.success_url)
+
         else:
             return self.form_invalid(form)
+
 
     def form_valid(self, form):
         response = super().form_valid(form)
@@ -64,47 +64,22 @@ class SignUpView(CreateView):
         user.groups.add(group)
         return response
 
+
     def form_invalid(self, form):
         return self.render_to_response(self.get_context_data(form=form))
 
 
+
+
 class UserRegisterCodeView(View):
     form_class = VerifyCodeForm
-    resend = False
-
-    @classmethod
-    def create(cls, resend=False):
-        instance = cls()
-        instance.resend = resend
-        return instance
-
-    def resend_code(self, user_session):
-        code_instance = OtpCode.objects.filter(email=user_session['email']).first()
-
-        if code_instance and (timezone.now() - code_instance.created_at) <= timedelta(minutes=2):
-            code_instance.delete()
-
-        new_otp_code = generate_otp_code()
-        OtpCode.objects.filter(email=user_session['email']).delete()
-        OtpCode.objects.create(email=user_session['email'], otp_code=new_otp_code)
-
-        send_otp_code(
-            recipient=user_session['email'],
-            subject='FAST FOODIA Verification Code',
-            message=f'Your verification code is: {new_otp_code}',
-        )
-        time_remaining = 120
-        return time_remaining
 
     def get(self, request):
         user_session = request.session['user_registration_info']
         print(44 * '=' + 'user_session' + 44 * '=')
         print(user_session)
         form = self.form_class
-        code_instance = OtpCode.objects.get(email=user_session['email'])
-        if self.resend:
-            self.resend_code(user_session)
-        return render(request, 'users/verify.html', {'form': form, 'code_instance': code_instance})
+        return render(request, 'users/verify.html', {'form': form})
 
     def post(self, request):
         user_session = request.session['user_registration_info']
@@ -125,26 +100,131 @@ class UserRegisterCodeView(View):
                 messages.error(request, 'Wrong Code!', 'danger')
                 return redirect('verify')
         return redirect('/')
-
-    def dispatch(self, request, *args, **kwargs):
-        if kwargs.get('resend', False):
-            user_session = request.session.get('user_registration_info', {})
-            self.resend_code(user_session)
-            return JsonResponse({'success': True, 'time_remaining': 120})
-        return super().dispatch(request, *args, **kwargs)
-
-
-class ResendCodeView(UserRegisterCodeView):
-    def get(self, request):
-        user_session = request.session.get('user_registration_info', {})
-        self.resend_code(user_session)
-        return JsonResponse({'success': True, 'time_remaining': 120})
-        if code_instance and (timezone.now() - code_instance.created_at) <= timedelta(minutes=2):
-            pass
-        else:
-            self.resend_code(user_session)
-
-        return JsonResponse({'success': True, 'time_remaining': 120})
+# ==============================================================================
+# class SignUpView(CreateView):
+#     model = get_user_model()
+#     form_class = CustomUserCreationForm
+#     template_name = 'users/sign_up.html'
+#     # template_name = 'index.html'
+#     # success_url = reverse_lazy('login')
+#     success_url = reverse_lazy('verify')
+#
+#     def post(self, request, *args, **kwargs):
+#         form = self.get_form()
+#         if form.is_valid():
+#             random_code = generate_otp_code()
+#             send_otp_code(
+#                 recipient=form.cleaned_data['email'],
+#                 subject='FAST FOODIA Verification Code',
+#                 message=f'Your verification code is: {random_code}',
+#             )
+#             OtpCode.objects.create(email=form.cleaned_data['email'], otp_code=random_code)
+#             request.session['user_registration_info'] = {
+#                 'username': form.cleaned_data['username'],
+#                 'email': form.cleaned_data['email'],
+#                 'phone': form.cleaned_data['phone'],
+#                 'password': form.cleaned_data['password1'],
+#             }
+#             messages.success(request, 'OTP Code Sent to your email.', 'success')
+#             return HttpResponseRedirect(self.success_url)
+#         else:
+#             return self.form_invalid(form)
+#
+#     def form_valid(self, form):
+#         response = super().form_valid(form)
+#         user = form.save(commit=False)
+#         customer = form.cleaned_data['is_customer']
+#         # if customer == True:
+#         #     group = Group.objects.get(name=_('Customer'))
+#         # if not customer:
+#         #     group = Group.objects.get(name=_('Employee'))
+#         group_name = _('Customer') if customer else _('Employee')
+#         group = Group.objects.get(name=group_name)
+#         user.save()
+#         user.groups.add(group)
+#         return response
+#
+#     def form_invalid(self, form):
+#         return self.render_to_response(self.get_context_data(form=form))
+#
+#
+# class UserRegisterCodeView(View):
+#     form_class = VerifyCodeForm
+#     resend = False
+#
+#     @classmethod
+#     def create(cls, resend=False):
+#         instance = cls()
+#         instance.resend = resend
+#         return instance
+#
+#     def resend_code(self, user_session):
+#         code_instance = OtpCode.objects.filter(email=user_session['email']).first()
+#
+#         if code_instance and (timezone.now() - code_instance.created_at) <= timedelta(minutes=2):
+#             code_instance.delete()
+#
+#         new_otp_code = generate_otp_code()
+#         OtpCode.objects.filter(email=user_session['email']).delete()
+#         OtpCode.objects.create(email=user_session['email'], otp_code=new_otp_code)
+#
+#         send_otp_code(
+#             recipient=user_session['email'],
+#             subject='FAST FOODIA Verification Code',
+#             message=f'Your verification code is: {new_otp_code}',
+#         )
+#         time_remaining = 120
+#         return time_remaining
+#
+#     def get(self, request):
+#         user_session = request.session['user_registration_info']
+#         print(44 * '=' + 'user_session' + 44 * '=')
+#         print(user_session)
+#         form = self.form_class
+#         code_instance = OtpCode.objects.get(email=user_session['email'])
+#         if self.resend:
+#             self.resend_code(user_session)
+#         return render(request, 'users/verify.html', {'form': form, 'code_instance': code_instance})
+#
+#     def post(self, request):
+#         user_session = request.session['user_registration_info']
+#         code_instance = OtpCode.objects.get(email=user_session['email'])
+#         form = self.form_class(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             if cd['code'] == code_instance.otp_code:
+#                 User = get_user_model()
+#                 user_instance = User.objects.create_user(user_session['email'], user_session['phone'],
+#                                                          user_session['password'])
+#                 user_instance.username = user_session['username']
+#                 user_instance.save()
+#                 code_instance.delete()
+#                 messages.success(request, "Your account Verified successfully", 'success')
+#                 return redirect('login')
+#             else:
+#                 messages.error(request, 'Wrong Code!', 'danger')
+#                 return redirect('verify')
+#         return redirect('/')
+#
+#     def dispatch(self, request, *args, **kwargs):
+#         if kwargs.get('resend', False):
+#             user_session = request.session.get('user_registration_info', {})
+#             self.resend_code(user_session)
+#             return JsonResponse({'success': True, 'time_remaining': 120})
+#         return super().dispatch(request, *args, **kwargs)
+#
+#
+# class ResendCodeView(UserRegisterCodeView):
+#     def get(self, request):
+#         user_session = request.session.get('user_registration_info', {})
+#         self.resend_code(user_session)
+#         return JsonResponse({'success': True, 'time_remaining': 120})
+#         if code_instance and (timezone.now() - code_instance.created_at) <= timedelta(minutes=2):
+#             pass
+#         else:
+#             self.resend_code(user_session)
+#
+#         return JsonResponse({'success': True, 'time_remaining': 120})
 
 
 # def login_view(request):
