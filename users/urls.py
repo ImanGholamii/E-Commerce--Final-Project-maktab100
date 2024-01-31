b55517171718fc8b7b2ceb0113e20f57a563bb6e
ImanGholami <imangholamiimi@gmail.com>
2024-02-01 00:18:00 +0330

    UPDATE| USERS | URLS | :zap: | password_reset urls

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from users.views import SignUpView, login_view, profile_view, Logout, home, UserRegisterCodeView, EmployeeSignUpView, \
-    EmployeeRegisterCodeView
+    EmployeeRegisterCodeView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name="signup"),
@@ -12,5 +12,9 @@ urlpatterns = [
     path('logout/', Logout.as_view(), name="logout"),
     path('users/profile/', profile_view, name="profile"),
     path('home/', home , name="home"),
+    path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
