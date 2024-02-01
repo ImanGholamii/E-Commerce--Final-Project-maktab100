aff363c9681f4ed7f69d8f28833f1c53e2bd7670
ImanGholami <imangholamiimi@gmail.com>
2024-02-02 02:09:26 +0330

    ADD | PRODUCTS | URLS | :tada:

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from products.views import ProductDetailView, HomeView, ProductListView, ProductCategoryListView
+from products.views import ProductDetailView, HomeView, ProductListView, ProductCategoryListView, ParentCategoryListView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
     path('products/', ProductListView.as_view(), name='list'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='detail'),
     path('products/category/<str:category_name>/', ProductCategoryListView.as_view(), name='product_category'),
+    path('products/parent-category/<str:category_name>/', ParentCategoryListView.as_view(), name='parent_category'),
 ]
