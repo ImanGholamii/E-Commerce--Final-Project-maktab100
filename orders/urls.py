cee728d565494a3cfd904ecc0ac346e9a61e5eab
ImanGholami <imangholamiimi@gmail.com>
2024-02-05 14:56:10 +0330

    ADD | ORDERS | URLS | :tada: | add cancel_order urls

--- orders/urls.py
+++ orders/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from .views import CreateShoppingCartView, ShoppingCartDetailView, AddProductToCartView
+from .views import CreateShoppingCartView, ShoppingCartDetailView, AddProductToCartView, CancelOrderView
 
 urlpatterns = [
-
     path('create-shopping-cart/', CreateShoppingCartView.as_view(), name='create-shopping-cart'),
-
     path('shopping-cart/', ShoppingCartDetailView.as_view(), name='shopping-cart'),
-
     path('add-product-to-cart/', AddProductToCartView.as_view(), name='add-product-to-cart'),
+    path('cancel-order/<int:order_id>/', CancelOrderView.as_view(), name='cancel-order'),
 ]
