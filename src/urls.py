cc12cd7ae1ced1305beaa13cceb26fc564be64d0
ImanGholami <imangholamiimi@gmail.com>
2024-02-05 14:42:40 +0330

    UPDATE | SRC | URLS | :zap: | add api/orders/ to urls

--- src/urls.py
+++ src/urls.py
@@ -30,7 +30,7 @@ urlpatterns = i18n_patterns(
     # APP
     path('', include('products.urls')),
     path('users/', include('users.urls')),
-    path('orders/', include('orders.urls')),
+    path('api/orders/', include('orders.urls')),
     path('email/', include('email_sender_app.urls')),
 
     # API
