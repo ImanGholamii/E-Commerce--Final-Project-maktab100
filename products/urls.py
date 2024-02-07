f51cbe308a85194de19ef7add560f6bc1cacd202
ImanGholami <imangholamiimi@gmail.com>
2024-02-07 12:29:08 +0330

    UPDATE | PRODUCTS | URLS | :zap:

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from products.views import ProductDetailView, HomeView, ProductListView, ProductCategoryListView, ParentCategoryListView
+from products.views import ProductDetailView, homeview, ProductListView, ProductCategoryListView, \
+    ParentCategoryListView, ProductDetailApiView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='index'),
+    path('', homeview, name='index'),
     path('products/', ProductListView.as_view(), name='list'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='detail'),
     path('products/category/<str:category_name>/', ProductCategoryListView.as_view(), name='product_category'),
     path('products/parent-category/<str:category_name>/', ParentCategoryListView.as_view(), name='parent_category'),
+    # API
+    path('api/product/<int:pk>/', ProductDetailApiView.as_view(), name='product_detail_api'),
+
 ]
