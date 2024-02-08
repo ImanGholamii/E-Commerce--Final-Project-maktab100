2bccb5cbd6bf3ef08f4f899db4b315bcc592efa5
ImanGholami <imangholamiimi@gmail.com>
2024-02-08 09:54:51 +0330

    ADD | ORDERS | SERIALIZERS | :tada: | orderitem

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from orders.models import Order
+from orders.models import Order, OrderItem
+
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
+        fields = '__all__'
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
         fields = '__all__'
