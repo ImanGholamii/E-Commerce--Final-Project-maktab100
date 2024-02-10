e7860add722741e4dfcfad8ea1c151dbb0765fd1
ImanGholami <imangholamiimi@gmail.com>
2024-02-10 21:08:33 +0330

    ADD | ORDERS | VIEWS | OrderUpdateDeleteView | :tada:

--- orders/views.py
+++ orders/views.py
@@ -7,6 +7,7 @@ from rest_framework.views import APIView
 
 
 class OrderApiVew(APIView):
+    """ GET and POST Orders"""
     def get(self, request):
         orders = Order.objects.all()
         serializer = OrderSerializer(orders, many=True)
@@ -22,6 +23,21 @@ class OrderApiVew(APIView):
             instance = serializer.save()
             instance.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class OrderUpdateDeleteView(APIView):
+    @swagger_auto_schema(
+        request_body=OrderSerializer,
+        responses={200: OrderSerializer()}
+    )
+    def put(self, request, pk):
+        order_obj = Order.objects.get(id=pk)
+        serializer = OrderSerializer(order_obj, data=request.data, partial=False)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
