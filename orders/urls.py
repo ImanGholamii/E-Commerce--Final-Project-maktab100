96c221ab6dad60da82fcd00d06e4b3cf9b1ba722
ImanGholami <imangholamiimi@gmail.com>
2024-02-10 21:40:29 +0330

    ADD | ORDERS | URLS | OrderItemApiView | :tada:

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import OrderApiVew, OrderUpdateDeleteView, OrderListCreateView
+from .views import OrderApiVew, OrderUpdateDeleteView, OrderItemApiView, OrderListCreateView
 
 urlpatterns = [
     path('', OrderApiVew.as_view()),
     path('<int:pk>/', OrderUpdateDeleteView.as_view()),
-    # path('', OrderListCreateView.as_view()),
+    path('items/', OrderItemApiView.as_view()),
+    # path('items/<int:pk>/', OrderUpdateDeleteView.as_view()),
+    # path('', OrderItemUpdateDeleteApiView.as_view()),
 ]
