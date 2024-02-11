df213af53554190e5973b270b7aa81f994fb2554
ImanGholami <imangholamiimi@gmail.com>
2024-02-11 12:32:00 +0330

    ADD | OrderItemApiView | POST | :tada: | Add or Remove items form Order

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,10 @@
+from django.shortcuts import get_object_or_404, render
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status, generics
 from rest_framework.response import Response
+
+from products.models import Product
+from users.models import Customer, User
 from .models import Order, OrderItem
 from .serializers import OrderSerializer, OrderItemSerializer
 from rest_framework.views import APIView
@@ -19,12 +23,16 @@ class OrderApiVew(APIView):
         responses={201: OrderSerializer()}
     )
     def post(self, request):
-        serializer = OrderSerializer(data=request.data)
-        if serializer.is_valid():
-            instance = serializer.save()
-            instance.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if not request.user.is_authenticated:
+            if 'guest_user' not in request.COOKIES:
+                request.COOKIES['guest_user'] = User.objects.get(id=45)
+                print(request.COOKIES)
+                serializer = OrderSerializer(data=request.data)
+                if serializer.is_valid():
+                    instance = serializer.save()
+                    instance.save()
+                    return Response(serializer.data, status=status.HTTP_201_CREATED)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class OrderUpdateDeleteView(APIView):
@@ -55,11 +63,56 @@ class OrderUpdateDeleteView(APIView):
 
 class OrderItemApiView(APIView):
     """GET and POST order items"""
+
     def get(self, request):
         order_item = OrderItem.objects.all()
         serializer = OrderItemSerializer(order_item, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    @swagger_auto_schema(
+        request_body=OrderItemSerializer,
+        responses={201: OrderItemSerializer()}
+    )
+    def post(self, request):
+        """Add or Remove items form Order(cart)"""
+        product_id = request.data.get('product')
+        quantities = request.data.get('quantities', 0)
+        print('product_id= ', product_id, quantities)
+        product = get_object_or_404(Product, id=product_id)
+
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            print('auth_user_id=', user_id)
+
+        else:
+            guest_user_id = request.COOKIES.get('guest_user')
+            print(guest_user_id)
+            if guest_user_id == 'guest':
+                guest_user_id = 45
+                user_id = int(guest_user_id)
+                print('guest_customer.id = ', user_id)
+        order, created = Order.objects.get_or_create(customer=user_id, status='pending')
+
+        order_item = order.order_items.filter(product=product).first()
+        if order_item:
+            order_item.quantities += quantities
+            if order_item.quantities <= 0:
+                order_item.delete()
+            else:
+                order_item.save()
+            serializer = OrderItemSerializer(order_item)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            serializer = OrderItemSerializer(data={'product': product_id, 'order': order.id, 'quantities': quantities})
+            if serializer.is_valid():
+                instance = serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def check_cart(request):
+    """to show all ordered items in template"""
+    return render(request, 'check_cart.html')
 
 # ==================
 class OrderListCreateView(generics.ListCreateAPIView):
