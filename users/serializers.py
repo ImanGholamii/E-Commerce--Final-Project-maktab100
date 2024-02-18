d60bd8fbe02fcdcc1c9ffe62b8f9c89e75be7388
ImanGholami <imangholamiimi@gmail.com>
2024-02-18 15:24:17 +0330

    ADD | USERS | SERIALIZERS | User | :tada:

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,11 @@
-from users.models import UserProfile
+from users.models import UserProfile, User
 from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
