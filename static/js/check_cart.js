956e20af2d74a7c551c996453f64afe9da0d2932
ImanGholami <imangholamiimi@gmail.com>
2024-02-20 22:45:39 +0330

    UPDATE | STATIC | JS | check_cart | :zap: | add message

--- static/js/check_cart.js
+++ static/js/check_cart.js
@@ -1,4 +1,13 @@
-function updateQuantity(productId, quantityChange) {
+function adjustQuantity(productId, amount) {
+    var messageElement = document.getElementById("message");
+    var message = amount > 0 ? "+1" : "-1";
+
+    // Set the message and color
+    messageElement.innerText = message;
+    messageElement.className = amount > 0 ? "message green" : "message red";
+
+    updateQuantity(productId, amount);
+    // AJAX request to update the quantity
     fetch('/en/api/orders/items/', {
         method: 'POST',
         headers: {
@@ -7,20 +16,20 @@ function updateQuantity(productId, quantityChange) {
         },
         body: JSON.stringify({
             product: productId,
-            quantities: quantityChange
+            quantities: amount
         })
     })
-        .then(response => response.json())
-        .then(data => {
-
-            document.getElementById('quantity-' + productId).innerText = data.quantities;
-        })
-        .catch(error => console.error('Error:', error));
+    .then(response => response.json())
+    .then(data => {
+        // Clear the message after 2 seconds
+        setTimeout(function() {
+            messageElement.innerText = "";
+            messageElement.className = "message"; // Reset the color
+        }, 2000);
+    })
+    .catch(error => console.error('Error:', error));
 }
 
-function adjustQuantity(productId, amount) {
-    updateQuantity(productId, amount);
-}
 
 
 function getCookie(name) {
