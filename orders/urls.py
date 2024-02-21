9769eeca22cfa426c96c6c97272dc8ac1f8d1092
ImanGholami <imangholamiimi@gmail.com>
2024-02-21 18:12:23 +0330

    ADD | ORDERS | URLS | :tada: | submit_order

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import OrderApiVew, OrderUpdateDeleteView, OrderItemApiView, OrderListCreateView, \
-    OrderItemUpdateDeleteApiView, check_cart
+    OrderItemUpdateDeleteApiView, check_cart, submit_order
 
 urlpatterns = [
     path('', OrderApiVew.as_view(), name='add-to-cart'),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('items/', OrderItemApiView.as_view()),
     path('items/<int:pk>', OrderItemUpdateDeleteApiView.as_view()),
     path('cart/', check_cart, name="cart"),
+    path('submitted/', submit_order, name="submit_order"),
 
 ]
